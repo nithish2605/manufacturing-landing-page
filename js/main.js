03fea518000aa0693aebe78e51ef6e023dcaedc3
nithishkumar <nithishvj2605@.com>
2025-07-28 17:02:39 +0530

    footer section started

--- js/main.js
+++ js/main.js
@@ -18,31 +18,44 @@ $(document).ready(function () {
     });
 });
 
-// Responsive nav bar using GSAP // 
+// Responsive nav bar // 
 var menubar=document.querySelector(".hamburger");
 var navlinks=document.querySelector(".nav-links");
 let navOpen = false;
+
+// menubar.addEventListener('click',()=>{
+//     navlinks.classList.toggle('show');
+// });
+
+// //when clicking outside the navlinks, close the navlink
+// document.addEventListener('click',function(e){
+//     if(!navlinks.contains(e.target)&&!menubar.contains(e.target)){
+//         navlinks.classList.remove("show");
+//     }
+// })
+
 menubar.addEventListener('click', (e) => {
-    // e.stopPropagation();
     if (!navOpen) {
         navlinks.classList.add("show");
-        gsap.to(navlinks, { x: 0, opacity: 1, duration: 0.1, ease: "power2.out" });
+        gsap.to(navlinks, { y: 0, opacity: 1, duration: 0.2, ease: "power2.out" });
         navOpen = true;
     } else {    
-        gsap.to(navlinks, { x: -130, opacity: 0, duration: 0.1, ease: "power2.in", onComplete: () => {
+        gsap.to(navlinks, { y: -500, opacity: 0, duration: 0.2, ease: "power2.in", onComplete: () => {
             navlinks.classList.remove("show");
             gsap.set(navlinks, { clearProps: "all" });
             navOpen = false;
         }});
     }
 });
+
 //when clicking outside the navlinks, close the navlink
 document.addEventListener('click', function(e) {
     if (navOpen && !navlinks.contains(e.target) && !menubar.contains(e.target)) {
-        gsap.to(navlinks, { x: -130, opacity: 0, duration: 0.1, ease: "power2.in", onComplete: () => {
+        gsap.to(navlinks, { x: 130, opacity: 0, duration: 0.1, ease: "power2.in", onComplete: () => {
             navlinks.classList.remove("show");
             gsap.set(navlinks, { clearProps: "all" });
             navOpen = false;
         }});
     }
 });
+
